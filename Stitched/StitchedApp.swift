16f97d1894ca92acfd95a3d10e25f333f742167b
ziontuiasoa <ztuiasoa@icloud.com>
2023-05-01 15:53:41 -0600

    Initial Commit

---
+++ Stitched/StitchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitchedApp.swift
+//  Stitched
+//
+//  Created by Zion Tuiasoa on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StitchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
