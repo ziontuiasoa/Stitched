0a508cad95bdae284243a6a803e21b9303849187
ziontuiasoa <ztuiasoa@icloud.com>
2023-09-06 16:53:00 -0600

    add stitch counter and reset functionality

---
+++ Stitched/Strings.swift
@@ -0,0 +1,16 @@
+//
+//  Strings.swift
+//  Stitched
+//
+//  Created by Zion Tuiasoa on 5/1/23.
+//
+
+import Foundation
+
+struct Strings {
+    
+    struct Images {
+        static let minusCircleFill = NSLocalizedString("minus.circle.fill", comment: "")
+        static let plusCircleFill =  NSLocalizedString("plus.circle.fill", comment: "")
+    }
+}
